6764888373c223f301064ff08f778dcefa435980
cheekykorkind <cheekorkind@gmail.com>
2020-06-21 05:52:33 +0000

    test with cloudwatch logs

--- terraform/aws-account-management/demo/main.tf
+++ terraform/aws-account-management/demo/main.tf
@@ -1,4 +1,16 @@
 provider "aws" {
   profile = "my-demo"
-  region = "ap-northeast-1"
+  region = "us-east-1"
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    cloudwatchlogs = "http://192.168.33.14:4566"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "example1" {
+  name = "/example1"
 }
