e1b31deda61f4b8cc91894a598c4415875547f66
cheekykorkind <cheekorkind@gmail.com>
2020-07-04 11:03:21 +0000

    edit entrypoint

--- rails/6/puma/entrypoint.sh
+++ rails/6/puma/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# sudo chown -R ec2-user /workspace/myapp
+sudo chown -R ec2-user /workspace/myapp
 
-# # Remove a potentially pre-existing server.pid for Rails.
-# rm -f ./tmp/pids/server.pid
-# rm -rf ./tmp/*
-# mkdir -p ./tmp/pids
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f ./tmp/pids/server.pid
+rm -rf ./tmp/*
+mkdir -p ./tmp/pids
 
-# yarn install --check-files
-# bundle exec rails db:create db:migrate
-# bundle exec rails assets:precompile
-# bundle exec rails s -p 3000 -b 0.0.0.0
+yarn install --check-files
+bundle exec rails db:create db:migrate
+bundle exec rails assets:precompile
+bundle exec rails s -p 3000 -b 0.0.0.0
